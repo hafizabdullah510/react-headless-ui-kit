74cc2fe597a71af6fcba39f79fb1359dd7c4faaf
Hafiz Muhammad Abdullah <72753811+hafizabdullah510@users.noreply.github.com>
2026-01-29 20:12:46 +0400

    feat: add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+// Component exports
+export { Button } from "./Button/Button";
+export { Input } from "./Input/Input";
+export { Select } from "./Select/Select";
+export { Spinner } from "./Spinner/Spinner";
+
+// Type exports
+export type { ButtonOrLinkProps } from "./Button/button.types";
+export type { InputProps } from "./Input/input.types";
+export type { SelectProps } from "./Select/select.types";
